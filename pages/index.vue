8756e451286ec8e4daaa932b2824a2b57316976f
gbrunosan <gbrunosan159@gmail.com>
2024-02-19 18:14:55 -0400

    change some features about showing pokemon

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,38 @@
 
   
   <div class="flex flex-col items-center bg-[#f0f0f0] h-auto min-h-screen" :class="{'travarScroll' : isOpen}">
-    <div class="flex justify-center items-center w-full max-h-[50px] bg-[#f7f7f7] shadow-md sticky top-0" :class="{hidden : isOpen}">
+    <div class="flex justify-center items-center w-full h-[50px] max-h-[50px] bg-[#f7f7f7] shadow-md sticky top-0" :class="{hidden : isOpen}">
       <img src="https://cdn.icon-icons.com/icons2/2248/PNG/512/pokeball_icon_136305.png" class=" max-w-10 max-h-10" :class="{hidden : isOpen}">
     </div>
     <div v-if="isLoading" class="flex h-screen justify-center items-center">
           <div  class="flex justify-center items-center animate-spin border-t-2 border-blue-700 border-solid rounded-full w-16 h-16"></div>
-      </div>
-    <div class="w-full max-w-[1000px] flex flex-col items-center" v-if="!isLoading">
+    </div>
+    <div class="w-full max-w-[1000px] flex flex-col items-center" v-show="!isLoading">
       <div class="flex justify-center items-center m-4 h-auto w-full sticky top-1" >
         <input type="text" placeholder="Pesquisar Pokémon" class="w-[80%] text-center rounded-lg p-2 shadow" :class="{hidden : isOpen}" v-model="pesquisa"/>
       </div>
       <div class="flex justify-center items-center">
         <div class="flex flex-wrap justify-center h-auto min-h-full gap-3">
-          <div  v-if="pesquisa === ''"  v-for="(pokemon, i) in pokemons" :key="i" @click="abrirModal(pokemon)" :class="pokemon.color, pokemon.hover" class="flex justify-center items-center flex-col rounded-lg min-h-[220px] h-auto w-[150px] md:w-[190px] md:min-h-[200px] shadow p-2">
+          <div  v-show="pesquisa === ''"  v-for="(pokemon, i) in loadMorePokemon" :key="i" @click="abrirModal(pokemon)" :class="pokemon.color, pokemon.hover" class="flex justify-center items-center flex-col rounded-lg min-h-[220px] h-auto w-[150px] md:w-[190px] md:min-h-[200px] shadow p-2">
               <img :src="pokemon.image" class="max-w-[120px] sm:max-w-[150px] md:max-w-[120px]"> 
               <p class="text-lg lg:text-xl text-black text-center">#{{ pokemon.id }} {{ pokemon.name }}</p>
-            </div>
+          </div>
+
           <div v-if="pesquisa !== ''" v-for="(pokemon, i) in filtroPokemon" :key="i" @click="abrirModal(pokemon)" :class="pokemon.color, pokemon.hover" class="flex justify-center items-center flex-col rounded-lg min-h-[220px] h-auto w-[150px] md:w-[190px] md:min-h-[200px] shadow p-2">
             <img :src="pokemon.image" class="max-w-[120px] sm:max-w-[150px] md:max-w-[120px]">
             <p class="text-lg lg:text-xl text-black text-center">#{{ pokemon.id }} {{ pokemon.name }}</p>    
           </div>
-        </div> 
+
+
+        </div>
       </div>
+
+      <div v-show="pesquisa===''" :class="{hidden : isOpen}" class="flex h-10 mt-3 justify-center items-center">
+              <div class="flex justify-center items-center animate-spin border-t-2 border-blue-700 border-solid rounded-full w-8 h-8"></div>         
+      </div>
+
+      <div v-show="pesquisa===''" :class="{hidden : isOpen}" class="w-4 h-4 flex justify-center items-center mt-[22px] bg[#ffffff]" ref="target"></div>
+
     </div>
   </div>
 </template>
@@ -34,8 +44,99 @@ import infoPokemon from '@/components/infoPokemon.vue'
 import { ref, onMounted, onBeforeUnmount, computed } from 'vue';
 import axios from 'axios';
 
+const isOpen = ref(false)
+const searchedPokemon = ref([])
+const pesquisa = ref('')
+const pokemonSelecionado = ref('')
+const isLoading = ref(false)
+const target = ref(null)
+const isVisible = ref(false)
 
-let intervaloCarregamento
+const limit = ref(0)
+
+const options = {
+  threshold: 0,
+  rootMargin: '0px',
+}
+
+const formatarNome = (str) => {
+  str = str.replace(/-/g, ' ');
+  return str.replace(/\b\w/g, match => match.toUpperCase());
+}
+
+onMounted(() => {
+    const observer = new IntersectionObserver(handleIntersection, options)
+    observer.observe(target.value)
+    filtrarPokemon()
+  });
+
+  async function filtrarPokemon (){
+  try {
+    const pokemonResponseFilter = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000`);
+    const filteredPokemon = pokemonResponseFilter.data.results;
+
+    for (const pokemon of filteredPokemon) {
+      const response = await axios.get(pokemon.url);
+      const filtered = {
+        id: response.data.id,
+        name: formatarNome(response.data.name),
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
+        gif: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${response.data.id}.gif`,
+        type: response.data.types,
+        color: typeColors[response.data.types[0].type.name.toLowerCase()],
+        hover: hoverTypeColors[response.data.types[0].type.name.toLowerCase()],
+        stats: response.data.stats,
+        abilities: response.data.abilities        
+      }
+
+      searchedPokemon.value.push(filtered)
+      
+    }
+  } catch (error) {
+    console.error('Erro ao carregar Pokémon:', error.message);
+  } 
+}  
+
+const loadMorePokemon = computed(() => {
+  return searchedPokemon.value.slice(0, limit.value);
+});
+
+const handleIntersection = (entries) => {
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      isVisible.value = true
+      limit.value += 30
+    }
+  })
+}
+
+
+
+const filtroPokemon = computed(() => {
+  return searchedPokemon.value.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(pesquisa.value.toLowerCase())
+  );
+});
+
+function abrirModal(pokemon) {
+  pokemon = JSON.parse(JSON.stringify(pokemon))
+  for (let type of pokemon.type){  
+    type.type.name = formatarNome(type.type.name)
+  } 
+  for (let ability of pokemon.abilities){  
+    ability.ability.name = formatarNome(ability.ability.name)
+  } 
+  pokemonSelecionado.value = pokemon
+  isOpen.value = true
+  travarScroll()
+}
+
+function travarScroll(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
 let typeColors = {
   water: 'bg-[#c0e2fc]',
   fire: 'bg-[#ffb778]',
@@ -78,87 +179,4 @@ let hoverTypeColors = {
   ground: 'hover:bg-[#c49352]'
 };
 
-const isOpen = ref(false)
-const pokemons = ref([]);
-const pesquisa = ref('');
-const pokemonSelecionado = ref('')
-const isLoading = ref(true);
-
-
-
-const carregarPokemons = async (offset, limit) => {
-  try {
-    const pokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-    const pokemonCarregados = pokemonResponse.data.results;
-
-    for (const pokemon of pokemonCarregados) {
-      const response = await axios.get(pokemon.url);
-      const newPokemon = {
-        id: response.data.id,
-        name: formatarNome(response.data.name),
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
-        gif: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${response.data.id}.gif`,
-        type: response.data.types,
-        color: typeColors[response.data.types[0].type.name.toLowerCase()],
-        hover: hoverTypeColors[response.data.types[0].type.name.toLowerCase()],
-        stats: response.data.stats,
-        abilities: response.data.abilities        
-      }
-      
-      pokemons.value.push(newPokemon)
-    }
-  } catch (error) {
-    console.error('Erro ao carregar Pokémon:', error.message);
-  } finally {
-      setTimeout(() => {
-      isLoading.value = false;
-    }, 150);
-  }
-};
-
-
-const formatarNome = (str) => {
-  str = str.replace(/-/g, ' ');
-  return str.replace(/\b\w/g, match => match.toUpperCase());
-}
-
-const carregarOutrosPokemon = () => {
-    intervaloCarregamento = setInterval(async () => {
-      const offset = pokemons.value.length;
-      await carregarPokemons(offset, 50);
-    }, 1800);
-  };
-
-onMounted(() => {
-  carregarOutrosPokemon();
-});
-
-onBeforeUnmount(() => {
-  clearInterval(intervaloCarregamento);
-});
-
-const filtroPokemon = computed(() => {
-  return pokemons.value.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(pesquisa.value.toLowerCase())
-  );
-});
-
-function abrirModal(pokemon) {
-  pokemon = JSON.parse(JSON.stringify(pokemon))
-  for (let type of pokemon.type){  
-    type.type.name = formatarNome(type.type.name)
-  } 
-  for (let ability of pokemon.abilities){  
-    ability.ability.name = formatarNome(ability.ability.name)
-  } 
-  pokemonSelecionado.value = pokemon
-  isOpen.value = true
-  travarScroll()
-}
-
-function travarScroll(){
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){window.scrollTo(x, y);};
-}
 </script>
